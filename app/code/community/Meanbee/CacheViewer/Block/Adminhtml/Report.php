81d35b04e140589a406061f9e5b3434a02a26de2
Nick Jones <nick@nicksays.co.uk>
2013-10-26 13:12:48 +0100

    Add headline stats and format the lifetime

--- app/code/community/Meanbee/CacheViewer/Block/Adminhtml/Report.php
+++ app/code/community/Meanbee/CacheViewer/Block/Adminhtml/Report.php
@@ -1,6 +1,8 @@
 <?php
 class Meanbee_CacheViewer_Block_Adminhtml_Report extends Mage_Adminhtml_Block_Template
 {
+    public $_total_cache_size_bytes = null;
+
     public function _construct()
     {
         parent::_construct();
@@ -12,6 +14,8 @@ class Meanbee_CacheViewer_Block_Adminhtml_Report extends Mage_Adminhtml_Block_Te
 
     public function getCacheItems()
     {
+        $this->_total_cache_size_bytes = 0;
+
         $cache_items = array();
 
         foreach ($this->_getCache()->getIds() as $cache_id) {
@@ -32,6 +36,8 @@ class Meanbee_CacheViewer_Block_Adminhtml_Report extends Mage_Adminhtml_Block_Te
             $cache_item->setSize($bytes);
             $cache_item->setTags($tags);
 
+            $this->_total_cache_size_bytes += $bytes;
+
             $cache_items[] = $cache_item;
         }
 
@@ -47,17 +53,58 @@ class Meanbee_CacheViewer_Block_Adminhtml_Report extends Mage_Adminhtml_Block_Te
     }
 
     /**
-     * Turn seconds into number of hours, minutes, days, etc.
+     * Turns seconds into 2m4d1h45m3s, for example.  If the year component is greater than zero, then we just
+     * return "forever".
      *
-     * @todo implement
      * @param int $raw_seconds
      * @return string
      */
     public function getAsTimeUnits($raw_seconds)
     {
-        $minutes = floor($raw_seconds/60);
-        $seconds = $raw_seconds % 60;
+        $date_interval_parts = array('y' => 'y', 'm' => 'm', 'd' => 'd', 'h' => 'h', 'i' => 'm', 's' =>'s');
+        $date_interval = Meanbee_CacheViewer_Model_DateInterval::createFromSeconds($raw_seconds);
+        $date_interval->recalculate();
 
-        return sprintf("%dm%ds", $minutes, $seconds);
+        $return_string = '';
+
+        if ($date_interval->y == 0) {
+            foreach ($date_interval_parts as $member => $output) {
+                if ($date_interval->$member > 0) {
+                    $return_string .= sprintf("%s%s", $date_interval->$member, $output);
+                }
+            }
+        } else {
+            $return_string = "forever";
+        }
+
+        return $return_string;
+    }
+
+    /**
+     * @return int
+     */
+    public function getCacheSizeInMb()
+    {
+        if ($this->_total_cache_size_bytes === null) {
+            $this->getCacheItems();
+        }
+
+        return ($this->_total_cache_size_bytes / 1024 / 1024);
+    }
+
+    /**
+     * @return int
+     */
+    public function getCacheFullPercentage()
+    {
+        return $this->_getCache()->getFillingPercentage();
+    }
+
+    /**
+     * @return string
+     */
+    public function getCacheBackend()
+    {
+        return get_class($this->_getCache()->getBackend());
     }
 }
---
+++ app/code/community/Meanbee/CacheViewer/Model/DateInterval.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Class Meanbee_CacheViewer_Model_DateInterval
+ *
+ * @see http://www.php.net/manual/en/dateinterval.format.php#102271
+ */
+class Meanbee_CacheViewer_Model_DateInterval extends DateInterval {
+
+    public static function createFromSeconds($seconds)
+    {
+        if ($seconds > 0) {
+            return new self("PT{$seconds}S");
+        } else {
+            return new self("PT0S");
+        }
+    }
+
+    /**
+     * @return int
+     */
+    public function to_seconds()
+    {
+        return ($this->y * 365 * 24 * 60 * 60) +
+        ($this->m * 30 * 24 * 60 * 60) +
+        ($this->d * 24 * 60 * 60) +
+        ($this->h * 60 * 60) +
+        ($this->i * 60) +
+        $this->s;
+    }
+
+    /**
+     * Recalculate interval parts
+     */
+    public function recalculate()
+    {
+        $seconds = $this->to_seconds();
+        $this->y = floor($seconds/60/60/24/365);
+        $seconds -= $this->y * 31536000;
+        $this->m = floor($seconds/60/60/24/30);
+        $seconds -= $this->m * 2592000;
+        $this->d = floor($seconds/60/60/24);
+        $seconds -= $this->d * 86400;
+        $this->h = floor($seconds/60/60);
+        $seconds -= $this->h * 3600;
+        $this->i = floor($seconds/60);
+        $seconds -= $this->i * 60;
+        $this->s = $seconds;
+    }
+}
