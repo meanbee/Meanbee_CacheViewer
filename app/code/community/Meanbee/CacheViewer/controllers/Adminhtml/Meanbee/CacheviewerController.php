e0f2049a5e4858d73e32dc186b996beae129389d
Nick Jones <nick@nicksays.co.uk>
2013-10-26 13:44:50 +0100

    add cache viewing

--- app/code/community/Meanbee/CacheViewer/controllers/Adminhtml/Meanbee/CacheviewerController.php
+++ app/code/community/Meanbee/CacheViewer/controllers/Adminhtml/Meanbee/CacheviewerController.php
@@ -7,4 +7,13 @@ class Meanbee_CacheViewer_Adminhtml_Meanbee_CacheviewerController extends Mage_A
         $this->getLayout()->getBlock('head')->setTitle("Meanbee CacheViewer");
         $this->renderLayout();
     }
+
+    public function view_singleAction()
+    {
+        $cache_id = $this->getRequest()->getParam('cache_id');
+
+        $cache_data = Mage::app()->getCache()->load($cache_id);
+
+        $this->getResponse()->setBody(($cache_data) ? $cache_data : '(false/empty)');
+    }
 }
